9d6b61026dc5d99fd1ba7e6bc693dab2a35f60a6
david <daviko@stud.ntnu.no>
2021-04-21 21:46:10 +0200

    Bump of modules version. Fix of DB connection.

--- assignment2.js
+++ assignment2.js
@@ -6,12 +6,30 @@ var mosca = require('mosca');
 require('dotenv').config()
 var settings = { port: 8080 }
 var broker = new mosca.Server(settings)
-
-// DB 
-var MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient
 
 // Broker started
 broker.on('ready', () => { console.log('broker ready') })
+
+// Database definition and connection
+const DATABASE = "Assignment2"
+const MSG_COLLECTION = "Messages"
+const PUB_COLLECTION = "Publishers"
+const SUB_COLLECTION = "Subscribers"
+const uri = process.env.DB_URL;
+const mongodbClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function runDB() {
+  try {
+    await mongodbClient.connect();
+
+    console.log("Connection to DB was successful.");
+
+    // await listDatabases(mongodbClient);
+  } finally {
+    // await mongodbClient.close();
+  }
+}
+runDB().catch(console.error);
 
 // Broker stores to DB when data is published to it.
 broker.on('published', (packet) => {
@@ -20,15 +38,13 @@ broker.on('published', (packet) => {
     if (packet.topic !== 'Bedroom' && packet.topic !== 'LivingRoom' && packet.topic !== 'Garage') return console.log('info:', packet.topic)
     const data = JSON.parse(payload)
     data.topic = packet.topic
-    MongoClient.connect(process.env.DB_URL, function (err, db) {
-        if (err) throw err;
 
-        db.collection("logs").insertOne(data, function (err, res) {
-            if (err) throw err;
-            // console.log("1 record inserted", DBEntry);
-            db.close();
-        });
-    });
+    const result = mongodbClient.db(DATABASE).collection(MSG_COLLECTION)
+      .insertOne(data)
+      .then(result => {
+        console.log(`A new record with id ${result.insertedId} was inserted to DB ${DATABASE} and table ${MSG_COLLECTION}.`);
+      })
+
 })
 
 
